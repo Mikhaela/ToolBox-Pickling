3d911368d7ccc7ff03ec35b23cfcbe8021537da3
Mikhaela Dietch <Mikhaela@Mikhaelas-MacBook-Pro.local>
2016-03-20 12:47:46 -0400

    final commit of tool box with comments

--- counter.py
+++ counter.py
@@ -33,34 +33,38 @@ def update_counter(file_name, reset=False):
 	2
 	"""
 
+	counter = 0 
 
-	if exists(file_name) == False or reset == True: # if file dne or reset = True
-		f = open(file_name,'w') # f is now a file object ready to be written
-		counter = 1
-		pickle.load(f)
-		f.write(str(counter)) # writes counter to file
+	# If file dne or reset = True we will create counter starting at 1 
+	if exists(file_name) == False or reset == True: 
+		f  = open(file_name,'w') # f is now a file object ready to be written
+		counter = 1 # set counter ready to write to file 
+		pickle.dump(counter,f)
+		return counter
 		f.close()
-		pickle.dump()
-		print 
 
-	elif exists(file_name) and reset == False: # if file exist and reset = False add one to counter
-		counter += 1 # adds 1 to pre-existing counter
-		f = open(file_name,'r+') # f is now a file object ready to be read
-		content = pickle.load(f) # load instead of dump as f already exists
+	# If file exist and reset = False we will add one to counter
+	elif exists(file_name) and reset == False: 
+		f = open(file_name,'r+') # f is now a file object ready to be read and then written 
+		counter = pickle.load(f) # load f as file already exists
 		f.seek(0,0) # moves pointer to start of file to write over previous content
-		f.write(str(counter)) # writes counter to file
-		f.close()
-
+		counter += 1 # adds 1 to pre-existing counter
+		pickle.dump(counter,f) # update counter in f 
+		return counter 
+		f.close() 
 		
-	
+if __name__ == '__main__':
+	if len(sys.argv) < 2:
+		import doctest
+		doctest.testmod()
+	else:
+		print "new value is " + str(update_counter(sys.argv[1])) # catches name variable 
 
-update_counter("blah.txt")
-		
-# if __name__ == '__main__':
-# 	if len(sys.argv) < 2:
-# 		import doctest
-# 		doctest.testmod()
-# 	else:
-# 		print "new value is " + str(update_counter(sys.argv[1])) # catches name variable 
+
+	# I had a hard time with this, my main difficulty was in sorting out the order of operations to call 
+	# 1) open/create file
+	# 2) if file contains content: load content with pickle
+	# 3) dump new content into file
+	# 4) return content & close file 
 
 
